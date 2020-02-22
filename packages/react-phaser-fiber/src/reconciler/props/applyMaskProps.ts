ee6965732b61886800f52f34127ed529b391853d
matt jennings <mattjennings44@gmail.com>
2020-02-22 10:53:56 -0700

    applyMaskProps

---
+++ packages/react-phaser-fiber/src/reconciler/props/applyMaskProps.ts
@@ -0,0 +1,23 @@
+import { iterateProps } from './iterateProps'
+import { MaskProps } from './types'
+
+/**
+ * Applies props for Phaser.GameObjects.Components.Mask
+ */
+export function applyMaskProps<T extends Phaser.GameObjects.Components.Mask>(
+  instance: T,
+  oldProps: MaskProps,
+  newProps: MaskProps
+) {
+  iterateProps(oldProps, newProps, (key, newValue) => {
+    switch (key) {
+      case 'mask':
+        if (newValue) {
+          instance.setMask(newValue)
+        } else {
+          instance.clearMask()
+        }
+        break
+    }
+  })
+}
