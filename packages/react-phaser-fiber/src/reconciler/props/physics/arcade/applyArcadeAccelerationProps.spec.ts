b5efd6e5189d48a8b4413972173dc7e8f4a68f56
matt jennings <mattjennings44@gmail.com>
2020-02-22 21:41:00 -0700

    applyArcadeAccelerationProps

---
+++ packages/react-phaser-fiber/src/reconciler/props/physics/arcade/applyArcadeAccelerationProps.spec.ts
@@ -0,0 +1,61 @@
+import { createGame } from '../../../../test-utils/createGame'
+import { applyArcadeAccelerationProps } from './applyArcadeAccelerationProps'
+
+describe('applyArcadeAccelerationProps', () => {
+  it('applies acceleration', async () => {
+    const game = await createGame()
+
+    const scene = game.scene.add('123', {}, true)
+    const instance = scene.physics.add.sprite(0, 0, null)
+    const spy = jest.spyOn(instance, 'setAcceleration')
+
+    applyArcadeAccelerationProps(
+      instance,
+      {},
+      {
+        acceleration: {
+          x: 1,
+          y: 1,
+        },
+      }
+    )
+
+    expect(spy).toHaveBeenCalledWith(1, 1)
+  })
+
+  it('applies accelerationX', async () => {
+    const game = await createGame()
+
+    const scene = game.scene.add('123', {}, true)
+    const instance = scene.physics.add.sprite(0, 0, null)
+    const spy = jest.spyOn(instance, 'setAccelerationX')
+
+    applyArcadeAccelerationProps(
+      instance,
+      {},
+      {
+        accelerationX: 1,
+      }
+    )
+
+    expect(spy).toHaveBeenCalledWith(1)
+  })
+
+  it('applies accelerationY', async () => {
+    const game = await createGame()
+
+    const scene = game.scene.add('123', {}, true)
+    const instance = scene.physics.add.sprite(0, 0, null)
+    const spy = jest.spyOn(instance, 'setAccelerationY')
+
+    applyArcadeAccelerationProps(
+      instance,
+      {},
+      {
+        accelerationY: 1,
+      }
+    )
+
+    expect(spy).toHaveBeenCalledWith(1)
+  })
+})
---
+++ packages/react-phaser-fiber/src/reconciler/props/physics/arcade/applyArcadeAccelerationProps.ts
@@ -0,0 +1,28 @@
+import { AccelerationProps } from '../../types'
+import { iterateProps } from '../../iterateProps'
+import isEqual from 'fast-deep-equal'
+
+/**
+ * Applies props for Phaser.Physics.Arcade.Components.Acceleration
+ */
+export function applyArcadeAccelerationProps<
+  T extends Phaser.Physics.Arcade.Components.Acceleration
+>(instance: T, oldProps: AccelerationProps, newProps: AccelerationProps) {
+  iterateProps(oldProps, newProps, (key, newValue, oldValue) => {
+    switch (key) {
+      case 'acceleration':
+        if (typeof newValue === 'number') {
+          instance.setAcceleration(newValue as number)
+        } else if (!isEqual(oldValue, newValue)) {
+          instance.setAcceleration(newValue.x, newValue.y)
+        }
+        break
+      case 'accelerationX':
+        instance.setAccelerationX(newValue as number)
+        break
+      case 'accelerationY':
+        instance.setAccelerationY(newValue as number)
+        break
+    }
+  })
+}
