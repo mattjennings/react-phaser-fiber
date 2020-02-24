9b26452baad00495c2bf8f81426bb5635906f2fb
matt jennings <mattjennings44@gmail.com>
2020-02-23 20:39:35 -0700

    fix tests randomly failing from scene not being ready

--- packages/react-phaser-fiber/src/test-utils/createGame.ts
+++ packages/react-phaser-fiber/src/test-utils/createGame.ts
@@ -2,12 +2,16 @@ import { wait } from '@testing-library/react'
 import Phaser from 'phaser'
 
 export async function createGame(config: Phaser.Types.Core.GameConfig = {}) {
+  const onSceneCreate = jest.fn()
   const game = new Phaser.Game({
     physics: {
       default: 'arcade',
     },
     type: Phaser.HEADLESS,
     banner: false,
+    scene: {
+      create: onSceneCreate,
+    },
     ...config,
   })
 
@@ -15,9 +19,12 @@ export async function createGame(config: Phaser.Types.Core.GameConfig = {}) {
     if (!game.isRunning) {
       throw 'Game not ready'
     }
+
+    // i can't figure out which event is emitted when a scene is ready (without loader), so this works for now
+    if (onSceneCreate.mock.calls.length === 0) {
+      throw 'Scene not ready'
+    }
   })
 
-  const scene = game.scene.add('main', {})
-
-  return { game, scene }
+  return { game, scene: game.scene.scenes[0] }
 }
