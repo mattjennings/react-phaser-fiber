eab75eb9dcf6525054aa225bc81bf9bedb3b27e1
matt jennings <mattjennings44@gmail.com>
2020-03-22 20:54:29 -0500

    add dummy index page

---
+++ docs/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function Index() {
+  return (
+    <Link to="/getting-started/installation/">
+      go to docs
+    </Link>
+  )
+}
